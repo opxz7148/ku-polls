30a597c33a81b24aba8e1c54cc8887b6ce8c9a34
Pasu Sangiemsin <opxz7148@gmail.com>
2024-08-25 17:11:26 +0700

    Add more test case and complete django tutorial 5

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,4 @@
+from wsgiref.headers import tspecials
 from django.db import models
 
 import datetime
@@ -10,11 +11,12 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.question_text
     
-    def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    def was_published_recently(self) -> bool:
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 
 class Choice(models.Model):
@@ -22,5 +24,5 @@ class Choice(models.Model):
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.choice_text
